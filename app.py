628e5c49604ff422916763378509e2b78e75e4a5
Zhuyi Xue <alfred532008@gmail.com>
2015-09-14 16:04:09 -0700

    multiprocessing with low memory usage works now

--- app.py
+++ app.py
@@ -7,25 +7,28 @@ import gzip
 import threading
 from multiprocessing import Queue, Process
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s|%(levelname)s|%(message)s')
+logging.basicConfig(level=logging.DEBUG, filename='app_serial.log',
+                    filemode='w',
+                    format='%(asctime)s|%(levelname)s|%(message)s')
 
 import numpy as np
 import pandas as pd
 
-from utils import pretty_usage, timeit, memo, split
+from utils import pretty_usage, timeit, memo, split_indexes
 from objs import BloomFilterBuilder
 
 
-PROB = 1e-9
 K_MER_SIZE = 20
 NUM_CPUS = 32         # given 32 cores
 
-def generate_bf_seqs():
-    """generate subarray of sequences needed to create a bloomfilter"""
+
+def generate_seqid_seqs():
+    """return "a list of tuples of (seqid, seq)"""
+
     input_ = '/projects/btl2/zxue/microorganism_profiling/libraries_assesment/gg_unite/gg_unite.csv.gz'
     logging.info('reading {0}'.format(input_))
     df = pd.read_csv(input_, compression='gzip')
-    # df = pd.read_csv(input_, compression='gzip', nrows=50)
+    # df = pd.read_csv(input_, compression='gzip', nrows=100)
     logging.info('reading Done')
 
     seqs = df.seq.values
@@ -33,14 +36,10 @@ def generate_bf_seqs():
                  'generated'.format(df.seq.shape[0]))
 
     # index each seq, 0-based index, ends up with tuples of (seq_id, seq)
-    id_seqs = zip(xrange(len(seqs)), seqs)
+    seqid_seqs = zip(xrange(len(seqs)), seqs)
+    return seqid_seqs
 
-    for ele in split(id_seqs):
-        # each element is of structure:
-        # (bf_id, [(seq_id1, seq1), (seq_id2, seq2), ..])
-        yield ele
 
-@memo
 def kmerize(seq, kmer_size):
     res = []
     for i in xrange(len(seq) + 1 - kmer_size):
@@ -64,122 +63,99 @@ def calc_hash_count_and_bf_size(n, p):
     return int(h), int(m)
 
 
-def calc_fp(m ,h, n):
-    """
-    Calculate false positive rate
-
-    :param m: size of bloomfilter
-    :param h: number of hash count
-    :param n: number of elements
-
-    ref: http://www.maxburstein.com/blog/creating-a-simple-bloom-filter/
-    """
-    return (1 - (1 - 1 / m) ** (h * n)) ** h
-
-# @timeit
-def generate_bf(id_seqs):
+def build_bf(id_seqs):
+    """just estimate number of uniq k_kmers to speed up processing"""
     if len(id_seqs) == 1:
         seq_id = id_seqs[0][0]
     else:
         seq_id = None
 
-    num_total_kmers = 0                 # include all kmers
-    uniq_kmers = []
+    calc_num_kmers = lambda seq: (len(seq) - K_MER_SIZE + 1)
+    num_kmers = sum(calc_num_kmers(x[1]) for x in id_seqs)
+
+    prob = 0.00075
+
+    hash_count, bf_size = calc_hash_count_and_bf_size(num_kmers, prob)
+    logging.info('bf_size: {0} ({1})'.format(bf_size, pretty_usage(bf_size / 8.)))
+    logging.info('hash_count: {0}'.format(hash_count))
+
+    # The number of unique kmers is approximate because it didn't take into
+    # account of same kmers amond different sequences. This is for saving
+    # memory and speed
+    num_total_kmers = 0
+    bf_builder = BloomFilterBuilder(bf_size, hash_count)
+    bf_add = bf_builder.add_uniq
     # _: don't track id if there are multiple sequences
-    for (_, seq) in id_seqs:
+    for k, (_, seq) in enumerate(id_seqs):
+        if (k + 1) % 10000 == 0:
+            logging.info('working on {0}th seqid_seqs'.format(k + 1))
         kmers = kmerize(seq, K_MER_SIZE)
         num_total_kmers += len(kmers)
-        # intermediate set to reduce memory usage
-        uniq_kmers.extend(list(set(kmers)))
+        kmers_set = set(kmers)
+        for __ in kmers_set:
+            bf_add(__)
 
-    uniq_kmers = set(uniq_kmers)    # final set
-    num_uniq_kmers = len(uniq_kmers)
+    logging.info('Total number of kmers: {0}'.format(num_total_kmers))
+    num_uniq_kmers = bf_builder.num_uniq_elems
+    logging.info("Number of uniq kmers: {0}".format(num_uniq_kmers))
 
-    # logging.info(num_total_kmers, num_uniq_kmers)
+    fpr = bf_builder.calc_fpr()
+    logging.info("Real false positive rate: {0}".format(fpr))
 
-    hash_count, bf_size = calc_hash_count_and_bf_size(num_uniq_kmers, PROB)
-    # logging.info(hash_count)
-    # logging.info(bf_size, pretty_usage(bf_size / 8.))
-    # logging.info("real false positive rate: {0}".format(calc_fp(bf_size, hash_count, num_uniq_kmers)))
-
-    bf_builder = BloomFilterBuilder(bf_size, hash_count)
-    for kmer in uniq_kmers:
-        bf_builder.add(kmer)
-
-    return bf_builder.bit_array, seq_id
+    return (num_uniq_kmers, bf_builder.size, bf_builder.hash_count,
+            fpr, bf_builder.bit_array, seq_id)
 
 
 def worker(queue, db_count):
     logging.info('connecting to db...')
     output_db = "db/{0}.db".format(db_count)
-    if output_db:
+    if os.path.exists(output_db):
         os.remove(output_db)
     conn = sqlite3.connect(output_db)
     cursor = conn.cursor()
 
     # cursor.execute("CREATE TABLE bloomfilter (bf_id INTEGER PRIMARY KEY, bitarray BLOB, FOREIGN KEY(seq_id) REFERENCES seq(id))")
-    cursor.execute("CREATE TABLE bloomfilter (bf_id INTEGER PRIMARY KEY, bitarray BLOB, seq_id)")
+    cursor.execute("CREATE TABLE bloomfilter (bf_id INTEGER PRIMARY KEY, num_uniq_kmers INTEGER, size INTEGER, hash_count INTEGER, fpr REAL, bitarray BLOB, seq_id)")
 
     counter = 0
     while True:
         bf_id, id_seqs = queue.get()
-        bf, seq_id = generate_bf(id_seqs)
-        if seq_id:
-            cursor.execute("INSERT INTO bloomfilter values (?, ?, ?)",
-                           (bf_id, sqlite3.Binary(bf.tobytes()), seq_id))
-        else:
-            cursor.execute("INSERT INTO bloomfilter values (?, ?, ?)",
-                           (bf_id, sqlite3.Binary(bf.tobytes()), None))
+        num_uniq_kmers, bf_size, bf_hash_count, bf_fpr, bf, seq_id = build_bf(id_seqs)
+        # seq_id is None if the bf is built on multiple sequences
+        cursor.execute("INSERT INTO bloomfilter values (?, ?, ?, ?, ?, ?, ?)",
+                       (bf_id, num_uniq_kmers, bf_size, bf_hash_count, bf_fpr,
+                        sqlite3.Binary(bf.tobytes()), seq_id))
         counter += 1
-        if counter % 500 == 0:
+        if counter % 10000 == 0:
             logging.info("commit {0} executions".format(counter))
             conn.commit()
 
         if queue.empty():
-            logging.info('db_count: {0}: waiting 5s for queue to be non-empty'.format(db_count))
-            time.sleep(5)
-            if queue.empty():   # still empty
-                logging.info('db_count: {0}: waiting 5s for queue to be non-empty'.format(db_count))
+            more_task = False   # indicate whether there are more tasks or not
+            n_loop = 10
+            for i in xrange(1, n_loop + 1):
+                logging.info('db_count: {0}: waiting {1}s for queue to be non-empty'.format(db_count, i))
+                time.sleep(i)
+                if not queue.empty():
+                    logging.info('db_count: {0}: more tasks found after {1}s '
+                                 'wait, back to work'.format(db_count, (i + 1) * i / 2))
+                    more_task = True
+                    break
+
+            if more_task:
+                continue
+            else:
+                total_seconds = (n_loop + 1) * n_loop / 2.
+                logging.info('db_count: {0}: still empty after {1}s wait for '
+                             'the queue, break the while loop'.format(
+                                 db_count, total_seconds))
                 break
     conn.commit()               # commit remaining execution
 
 
-@timeit
-def put_to_db(res_queue, output):
-    logging.info('connecting to db...')
-    conn = sqlite3.connect(":memory:")
-    cursor = conn.cursor()
-
-    # cursor.execute("CREATE TABLE bloomfilter (bf_id INTEGER PRIMARY KEY, bitarray BLOB, FOREIGN KEY(seq_id) REFERENCES seq(id))")
-    cursor.execute("CREATE TABLE bloomfilter (bf_id INTEGER PRIMARY KEY, bitarray BLOB, seq_id)")
-
-    while not res_queue.empty():
-        bf_id, bf, seq_id = res_queue.get()
-        # logging.info('putting {0}th bf with seq_id {1} into db'.format(bf_id, seq_id))
-        if seq_id:
-            cursor.execute("INSERT INTO bloomfilter values (?, ?, ?)",
-                           (bf_id, sqlite3.Binary(bf.tobytes()), seq_id))
-        else:
-            cursor.execute("INSERT INTO bloomfilter values (?, ?, ?)",
-                           (bf_id, sqlite3.Binary(bf.tobytes()), None))
-        res_queue.task_done()
-
-    logging.info('committing db')
-    conn.commit()
-    logging.info('dumping db to {0}'.format(output))
-    dump_db(conn, output)
-    conn.close()
-
-
-@timeit
-def dump_db(conn, output):
-    with gzip.open(output, 'w') as opf:
-        for line in conn.iterdump():
-            opf.write('%s\n' % line)
-
-    
 def main():
-    iter_bf_seqs = generate_bf_seqs()
+    seqid_seqs = generate_seqid_seqs()
+    bfid_beg_end_generator = split_indexes(0, len(seqid_seqs))
 
     queue = Queue()
 
@@ -190,10 +166,11 @@ def main():
         procs.append(proc)
         proc.start()
 
-    for k, (bf_id, id_seqs) in enumerate(iter_bf_seqs):
-        if (k + 1) % 10000 == 0:
+    for k, (bf_id, beg, end) in enumerate(bfid_beg_end_generator):
+        interval = 10 ** (len(str(k)) - 1)
+        if (k + 1) % interval == 0:
             logging.info('enqueuing {0}th bf'.format(k + 1))
-        queue.put((bf_id, id_seqs))
+        queue.put((bf_id, seqid_seqs[beg: end]))
     logging.info('enqueued {0}th bf'.format(k + 1))
 
     for proc in procs:
