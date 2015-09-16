5f7dfee68a74b78b42672d3070491748d0f37c17
Zhuyi Xue <alfred532008@gmail.com>
2015-09-16 13:41:10 -0700

    adapted utils functions to trees with arbitrary branches

--- test_split.py
+++ test_split.py
@@ -10,6 +10,13 @@ def test_calc_total_num_nodes_above():
     assert U.calc_total_num_nodes_above(3) == 7
 
 
+def test_calc_total_num_nodes_above_quaternary():
+    assert U.calc_total_num_nodes_above(0, 4) == 0
+    assert U.calc_total_num_nodes_above(1, 4) == 1
+    assert U.calc_total_num_nodes_above(2, 4) == 5
+    assert U.calc_total_num_nodes_above(3, 4) == 21
+
+
 def test_calc_level():
     assert U.calc_level(0) == 0
     assert U.calc_level(1) == 1
@@ -29,6 +36,26 @@ def test_calc_level():
     assert U.calc_level(15) == 4
 
 
+def test_calc_level_quaternary():
+    assert U.calc_level(0, 4) == 0
+    assert U.calc_level(1, 4) == 1
+    assert U.calc_level(2, 4) == 1
+    assert U.calc_level(3, 4) == 1
+    assert U.calc_level(4, 4) == 1
+    assert U.calc_level(5, 4) == 2
+    assert U.calc_level(6, 4) == 2
+    assert U.calc_level(7, 4) == 2
+    assert U.calc_level(8, 4) == 2
+    assert U.calc_level(9, 4) == 2
+    assert U.calc_level(10, 4) == 2
+    assert U.calc_level(11, 4) == 2
+    assert U.calc_level(12, 4) == 2
+    assert U.calc_level(13, 4) == 2
+    assert U.calc_level(14, 4) == 2
+    assert U.calc_level(15, 4) == 2
+    assert U.calc_level(21, 4) == 3
+
+
 def test_ith_node():
     assert U.calc_ith_node(0, 0) == 1
     assert U.calc_ith_node(1, 1) == 1
@@ -41,13 +68,44 @@ def test_ith_node():
     assert U.calc_ith_node(13, 3) == 7
 
 
+def test_ith_node_quaternary():
+    assert U.calc_ith_node(0, 0, 4) == 1
+    assert U.calc_ith_node(1, 1, 4) == 1
+    assert U.calc_ith_node(2, 1, 4) == 2
+    assert U.calc_ith_node(3, 1, 4) == 3
+    assert U.calc_ith_node(4, 1, 4) == 4
+    assert U.calc_ith_node(5, 2, 4) == 1
+    assert U.calc_ith_node(6, 2, 4) == 2
+    assert U.calc_ith_node(7, 2, 4) == 3
+    assert U.calc_ith_node(13, 2, 4) == 9
+
+    # with no level information
+    assert U.calc_ith_node(0, nbr=4) == 1
+    assert U.calc_ith_node(1, nbr=4) == 1
+    assert U.calc_ith_node(2, nbr=4) == 2
+    assert U.calc_ith_node(3, nbr=4) == 3
+    assert U.calc_ith_node(4, nbr=4) == 4
+    assert U.calc_ith_node(5, nbr=4) == 1
+    assert U.calc_ith_node(6, nbr=4) == 2
+    assert U.calc_ith_node(7, nbr=4) == 3
+    assert U.calc_ith_node(13, nbr=4) == 9
+
+
 def test_calc_children_id():
+    # binary
     assert U.calc_children_id(0, 0) == (1, 2)
     assert U.calc_children_id(2, 1) == (5, 6)
     assert U.calc_children_id(3, 2) == (7, 8)
     assert U.calc_children_id(5, 2) == (11, 12)
     assert U.calc_children_id(6, 2) == (13, 14)
     assert U.calc_children_id(7, 3) == (15, 16)
+
+    # quarternary
+    assert U.calc_children_id(0, 0, 4) == (1, 2, 3, 4)
+    assert U.calc_children_id(1, 1, 4) == (5, 6, 7, 8)
+    assert U.calc_children_id(2, 1, 4) == (9, 10, 11, 12)
+    assert U.calc_children_id(5, 2, 4) == (21, 22, 23, 24)
+    assert U.calc_children_id(6, 2, 4) == (25, 26, 27, 28)
 
 
 def test_split():
@@ -96,8 +154,13 @@ def test_split_indexes():
 
 
 test_calc_total_num_nodes_above()
+test_calc_total_num_nodes_above_quaternary()
 test_calc_level()
+test_calc_level_quaternary()
 test_ith_node()
+test_ith_node_quaternary()
 test_calc_children_id()
-test_split()
-test_split_indexes()
+# test_split()
+# test_split_indexes()
+
+
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import time
+import math                     # numpy log doesn't take arbitray base
 import numpy as np
 from functools import update_wrapper
 
@@ -73,9 +74,13 @@ def split(array, current_id=0, current_level=0):
 
 
 
-def split_indexes(beg, end, current_id=0, current_level=0):
-    """only return indexes to save memory usage"""
-    """split array recursively, keeping track of subarray ids, starting from 0"""
+def split_indexes(beg, end, current_id=0, current_level=0, n_branches=2):
+    """only return indexes to save memory usage
+
+    split array recursively, keeping track of subarray ids, starting from 0
+
+    :param b: when it's 2, it's a binary tree
+    """
     delta = end - beg
     if delta == 1:
         return
@@ -109,37 +114,66 @@ def split_indexes(beg, end, current_id=0, current_level=0):
 #         return 
 
 
-def calc_total_num_nodes_above(level):
+def calc_total_num_nodes_above(level, nbr=2):
+    """
+    :param nbr: number of branches
+    """
     if level < 0:
         raise ValueError('level cannot be below 0: {0}'.format(level))
 
-    if level == 0:
+    if level == 0:              # there is no node above level 0
         return 0
 
-    return 2 ** level - 1
+    # # because it's above level
+    # level -= 1
+    # res = (nbr ** (level + 1) - 1) // (nbr - 1)
+
+    # combining the above two commented lines since level doesn't change
+    # effectively
+    res = (nbr ** (level) - 1) // (nbr - 1)
+
+    # print level, res
+    return res
 
 
-def calc_level(current_id):
-    """given an id, calculate its level"""
-    return int(np.log2(current_id + 1))
+def calc_level(cid, nbr=2):
+    """given an current id (cid), calculate its level"""
+    b, n  = nbr, cid
+    if n == 0:
+        return 0
+    l =  math.log((n + 1) * (b - 1) + 1, b) - 1
+    if l == int(l):
+        return l
+    elif l > int(l):
+        return int(l + 1)
+    else:
+        raise ValueError('l < int(l) with l equals {0}'.format(r))
 
 
-def calc_ith_node(current_id, level):
-    return current_id - calc_total_num_nodes_above(level) + 1
+def calc_ith_node(cid, level=None, nbr=2):
+    """calculate this is the ith node at this level"""
+    if level is None:
+        level = calc_level(cid, nbr)
+    res = cid - calc_total_num_nodes_above(level, nbr) + 1
+    # print res
+    return res
 
 
-def calc_children_id(current_id, current_level):
-    if current_level == 0 and current_id == 0:
-        return (1, 2)
+def calc_children_id(cid, current_level=None, nbr=2):
+    if current_level is None:
+        current_level = calc_level(cid, nbr)
 
-    num_nodes_above = calc_total_num_nodes_above(current_level)
+    num_nodes_above = calc_total_num_nodes_above(current_level, nbr)
     # i: ith node at the current_level, +1 because of 0-based index
-    i = current_id - num_nodes_above + 1
+    ith = cid - num_nodes_above + 1
 
-    num_nodes_at_current_level = 2 ** current_level
-    c2 = 2 * i + (num_nodes_above + num_nodes_at_current_level - 1)
-    c1 = c2 - 1
-    return c1, c2
+    num_nodes_at_current_level = nbr ** current_level
+    nth_children = nbr * ith + (num_nodes_above + num_nodes_at_current_level - 1)
+    children = [nth_children]
+    for i in xrange(1, nbr):
+        children.append(nth_children - i)
+    children.reverse()
+    return tuple(children)
 
 
 def kmerize(seq, kmer_size):
